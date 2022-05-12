c47f050af2b54a25c1a28986f27cbe5dd6eb9fea
Swathi <71275010+mylearnings1@users.noreply.github.com>
2022-05-12 15:31:46 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -6,20 +6,27 @@ from io import BytesIO
  
 app = FastAPI()
  
-model_path = 'model_rf.jbl'
-loaded_model = None
- 
+model_path1 = 'model_rf.jbl'
+loaded_model1 = None
+model_path2 = 'fatures_selected.jbl'
+loaded_model2 = None
+
 @app.post("{full_path:path}")
 async def predict(data: UploadFile = File(...)):
     img = pd.read_csv(BytesIO(await data.read()))
     img = img.drop(columns = ['Time'], axis = 1)
-    global loaded_model
+    global loaded_model2
+    
+    loaded_model2 = joblib.load(model_path2)
+    img = pd.DataFrame(img, columns=loaded_model2)
+
+    global loaded_model1
     # Check if model is already loaded
  
-    if not loaded_model:
-        loaded_model = joblib.load(model_path)
+    if not loaded_model1:
+        loaded_model1 = joblib.load(model_path1)
  
     # Predict with the model
-    prediction = loaded_model.predict(img)
+    prediction = loaded_model1.predict(img)
  
     return f'Predicted_Value: {prediction}'
