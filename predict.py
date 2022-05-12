a2a1951414471959f91168601d6880a0fd3e9fde
Swathi <71275010+mylearnings1@users.noreply.github.com>
2022-05-12 11:32:50 +0530

    Create predict.py

---
+++ predict.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, File, UploadFile
+import pandas as pd
+import numpy
+import joblib
+from io import BytesIO
+ 
+app = FastAPI()
+ 
+model_path = 'model_rf.pckl'
+loaded_model = None
+ 
+@app.post("{full_path:path}")
+async def predict(data: UploadFile = File(...)):
+    img = pd.read_csv(BytesIO(await data.read()))
+
+    global loaded_model
+    # Check if model is already loaded
+ 
+    if not loaded_model:
+        loaded_model = joblib.load(model_path)
+ 
+    # Predict with the model
+    prediction = loaded_model.predict(img)
+ 
+    return f'Predicted_Flower: {prediction}'
